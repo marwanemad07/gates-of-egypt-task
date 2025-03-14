7a22a0675f63b14c427c8c6926a78602f29b47db
Marwan Emad <marwanemad910@gmail.com>
2025-03-14 15:25:14 +0200

    Use env variable to replace connection string

--- ECommerceApp.Infrastructure/Extensions/InfrastructureServiceRegistration.cs
+++ ECommerceApp.Infrastructure/Extensions/InfrastructureServiceRegistration.cs
@@ -4,7 +4,7 @@
     {
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration cfg)
         {
-            string connectionString = cfg.GetConnectionString("DockerConnection");
+            string connectionString = cfg.GetConnectionString("DefaultConnection");
             services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseNpgsql(connectionString);
